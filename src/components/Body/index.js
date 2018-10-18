3634058d60ce9fa4f642538a26756177f6ecaaf7
dalaoque <410009509@qq.com>
2018-10-18 17:18:33 +0800

    test lint-staged

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -1,49 +1,49 @@
-import React from "react";
-import showdown from "showdown";
-import image from "./image.jpg";
-import changelog from "./CHANGELOG.md";
+import React from 'react'
+import showdown from 'showdown'
+import image from './image.jpg'
+import changelog from './CHANGELOG.md'
 
 export default class extends React.PureComponent {
     constructor(props) {
-        super(props);
+        super(props)
         this.state = {
-            html: ""
-        };
+            html: ''
+        }
     }
 
     componentDidMount() {
-        this.getMarkdownFile();
+        this.getMarkdownFile()
     }
 
     componentWillUnmount() { }
 
     getMarkdownFile() {
-        sfetch(changelog)
+        fetch(changelog)
             .then(res => {
-                return res.text();
+                return res.text()
             })
             .then(data => {
-                const converter = new showdown.Converter();
+                const converter = new showdown.Converter()
 
                 /* eslint-disable react/no-danger */
                 const html = (
                     <div dangerouslySetInnerHTML={{ __html: converter.makeHtml(data) }} />
-                );
+                )
 
-                this.setState({ html });
+                this.setState({ html })
             })
             .catch(err => {
-                sconsole.log("err: ", err);
-            });
+                console.log('err: ', err)
+            })
     }
 
     render() {
-        const { html } = this.state;
+        const { html } = this.state
         return (
             <>
                 <img width={200} height={100} src={image} />
                 {html}
             </>
-        );
+        )
     }
 }
