030f6274cd088237ef36c9e305f105c9a8553a5a
John <410009509@qq.com>
2019-01-14 10:47:00 +0800

    git commit -m '更改package.json scripts & 更改webpack配置目录结构 创建webpackrc配置文件 & public静态文件配置待开发' (#7)
    * test lint-staged
    
    * init eslint
    
    * adsf
    
    * adsf
    
    * engines
    
    * prettier format code & React ESLint
    
    * test lint-stages
    
    * test1
    
    * test2
    
    * test3
    
    * test5
    
    * test6
    
    * test6
    
    * test7
    
    * test9
    
    * add postcss for first
    
    * some descriptions
    
    * 更改package.json scripts & 更改webpack配置目录结构 创建webpackrc配置文件 & public静态文件配置待开发

--- build/webpack/dev.js
+++ build/webpack/dev.js
@@ -1,7 +1,9 @@
 const merge = require('webpack-merge')
-const common = require('./webpack.common.js')
+const common = require('./common.js')
 const webpack = require('webpack')
 const openBrowser = require('react-dev-utils/openBrowser')
+const packageConfig = require('../../package.json')
+const { proxy } = require('../webpackrc')
 require('colors')
 
 module.exports = merge(common, {
@@ -13,15 +15,21 @@ module.exports = merge(common, {
         hot: true, // 热更新
         clientLogLevel: 'none', // 阻止浏览器console消息
         noInfo: true, // 启动时和每次保存之后，那些显示的 webpack 包(bundle)的消息将被隐藏。错误和警告仍然会显示。
-        proxy: {
-            '/site_api/v1': process.env.API_ROOT || '/'
-        },
+        proxy,
         after() {
             // 在服务内部的所有其他中间件之后， 提供执行自定义中间件的功能
-            const URI = 'http://localhost:3000'
-            console.log('Project is running at', `${URI}`.yellow)
+            console.log(serverStatus)
             openBrowser(URI)
         }
     },
     plugins: [new webpack.HotModuleReplacementPlugin()]
 })
+
+const URI = 'http://localhost:3000'
+
+const serverStatus = `
+===================
+    local_service: ${URI.yellow}
+     project_name: ${packageConfig.name || ''}
+===================
+`
--- build/webpack/prod.js
+++ build/webpack/prod.js
@@ -1,14 +1,14 @@
 const merge = require('webpack-merge')
 const path = require('path')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
-const common = require('./webpack.common.js')
+const common = require('./common.js')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = merge(common, {
     mode: 'production',
     plugins: [
         new CleanWebpackPlugin(['dist'], {
-            root: path.resolve(__dirname, '..')
+            root: path.resolve(__dirname, '../..')
         }),
         new UglifyJSPlugin()
     ]
